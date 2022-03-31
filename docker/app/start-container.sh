57d4db7c0a498148d8779d3dd3922e224d7fa444
ezeveliz <ezequiel_veliz@hotmail.com>
2022-03-31 15:15:44 -0300

    * Agrego un par de comentarios en el script start-container

--- docker/app/start-container.sh
+++ docker/app/start-container.sh
@@ -3,12 +3,15 @@
 # La linea de arriba encuentra y ejecuta bash para despues continuar con lo de abajo
 
 # Me aseguro que el directorio de composer exista y que sea modificable
+# -d: indica si existe el archivo y el mismo es un directorio
 if [ ! -d /.composer ]; then
     mkdir /.composer
 fi
 
 chmod -R ugo+rw /.composer
 
+# $#: numero de parametros ingresados
+# -gt: greater than
 if [ $# -gt 0 ]; then
 
   # Si pase algun parametro( y posibles flags al docker run...) lo ejecuto
